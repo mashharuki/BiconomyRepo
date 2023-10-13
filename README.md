08d9dac395b7ca6f3f95484d0b2449ad125ba5cb
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-10-13 19:08:06 +0900

    サンプルコードを追加

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Biconomy 調査・研究のためのリポジトリです。
 2. [eth のエンドポイント](https://www.ankr.com/rpc/eth)
 3. [エンドポイント一覧](https://www.ankr.com/rpc/)
 4. [Polygon ZKEVM のエンドポイント](https://www.ankr.com/rpc/polygon_zkevm)
+5. [create-a-new-paymaster API](https://docs.biconomy.io/docs/apireference/dashboardapi#2-create-a-new-paymaster)
---
+++ sdk-examples/backend-node/scripts/address.ts
@@ -0,0 +1,9 @@
+const chalk = require('chalk')
+import { createBiconomyAccountInstance } from "./helperFunctions";
+import config from "../config.json";
+
+export async function getAddress() {
+  const biconomySmartAccount = await createBiconomyAccountInstance();
+  const scwAddress = await biconomySmartAccount.getAccountAddress();
+  console.log(chalk.green(`SmartAccount address: ${scwAddress}`));
+}
