2868458334a925c6add584b9daa067f71109c97b
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-10-17 10:34:20 +0900

    名前を変更

--- safe-lit-pkp-dapp2/src/components/Minter.tsx
+++ safe-lit-pkp-dapp2/src/components/Minter.tsx
@@ -61,47 +61,10 @@ const Minter: React.FC<Props> = ({ biconomyService, smartAccount, address, provi
     });
   }
 
-  /**
-   * handleMint2
-   */
-  const handleMint2 = async () => {
-    toast.info('Minting your CrossChain NFT...', {
-      position: "top-right",
-      autoClose: 15000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark",
-     });
-
-    // call mintNFT method
-    const transactionHash = await biconomyService.crossMintNft(
-      smartAccount, 
-      provider, 
-      address
-    );
-
-    setMinted(true)
-
-    toast.success(`Success! Here is your transaction: https://ccip.chain.link/msg/${transactionHash} `, {
-      position: "top-right",
-      autoClose: 18000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark",
-    });
-  }
-
 
   return(
     <>
       {address && <button onClick={handleMint} className={styles.connect}>Mint NFT</button>}
-      {address && <button onClick={handleMint2} className={styles.connect}>Mint CrossChain NFT</button>}
       {minted && <a href={`https://testnets.opensea.io/${address}`}> Click to view minted nfts for smart account</a>}
       <ToastContainer
         position="top-right"
